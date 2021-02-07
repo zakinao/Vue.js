d83473d172831adb7ac6233927203a97bba78e85
zakinao <zakinaororo@gmail.com>
2021-02-07 14:13:29 +0900

    custom directive

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,22 @@
 <template>
-  <p>Home</p>
+  <p v-border:solid.round.shadow="{width:'5px', color:'red'}">Home</p>
 </template>
+
+<script>
+export default {
+  directives: {
+      border(el, binding) {
+        el.style.border = "solid black 2px";
+        el.style.borderWidth = binding.value.width;
+        el.style.borderColor = binding.value.color;
+        el.style.borderStyle = binding.arg;
+        if (binding.modifiers.round) {
+          el.style.borderRadius = "0.5rem";
+        }
+        if (binding.modifiers.shadow) {
+          el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+        }
+      }
+  }
+}
+</script>
