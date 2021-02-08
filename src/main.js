e95e8680146c25f88ab87213b8f5a322522374a6
zakinao <zakinaororo@gmail.com>
2021-02-08 18:19:02 +0900

    vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ router.beforeEach((to, from, next) => {
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,8 +3,24 @@
     <h2>Home</h2>
     <router-link to="/" exact-active-class="link--active" class="link">Home</router-link>
     <router-link to="/users" exact-active-class="link--active" class="link">Users</router-link>
+    <button @click="increment(2)">+1</button>
+    <button @click="decrement(2)">-1</button>
   </nav>
 </template>
+
+<script>
+
+export default {
+  methods: {
+    increment() {
+      this.$store.dispatch('count/increment', 2);
+    },
+    decrement() {
+      this.$store.dispatch('count/decrement', 2);
+    }
+  }
+};
+</script>
 
 <style scoped>
   .link--active {
